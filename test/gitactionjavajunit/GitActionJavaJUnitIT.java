d92c3a31c7adb48f66f3118e5f7d1da89525fe80
Maicon dos Santos <santos.maicon@gmail.com>
2024-11-08 17:03:17 -0300

    Add files via upload

---
+++ test/gitactionjavajunit/GitActionJavaJUnitIT.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package gitactionjavajunit;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Maicon
+ */
+public class GitActionJavaJUnitIT {
+
+    @Test
+    public void testSomeMethodTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testSomeMethodFalse() {
+        assertFalse(true);
+    }
+    
+}
